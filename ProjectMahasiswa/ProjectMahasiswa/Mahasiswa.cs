5e7952300e8d51ae68d2f1f8a685ea7b5054b143
Yoga Adi Putra <91592424+YogaAdiPutra@users.noreply.github.com>
2022-07-15 10:23:41 +0700

    Add files via upload

---
+++ ProjectMahasiswa/ProjectMahasiswa/Mahasiswa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectMahasiswa
+{
+    public class Mahasiswa 
+    {
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public string Jenis_Kelamin { get; set; }
+    }
+    static void Main(string[] args)
+    {
+        Mahasiswa mhs1 = new Mahasiswa();
+        mhs1.Nim = "18.11.2464";
+        mhs1.Nama = "YogaAdiP";
+        mhs1.Jenis_Kelamin = "Laki-laki";
+
+        List<Mahasiswa> List = new List<Mahasiswa>();
+
+        List.Add(mhs1);
+
+        Mahasiswa mhs = List[];
+        Console.WriteLine("{0},{1} , mhs.Nim, mhs.Nama, mhs.Jenis_Kelamin);
+
+        Console.ReadKey();
+
+    }
+}
---
+++ ProjectMahasiswa/ProjectMahasiswa/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectMahasiswa
+{
+    class Program
+    {
+        // deklarasi objek collection untuk menampung objek mahasiswa
+        static List<string> daftarMahasiswa = new List<string>();
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Responsi UAS Matakuliah Pemrograman";
+
+            while (true)
+            {
+                TampilMenu();
+
+                Console.Write("\nNomor Menu [1..3]: ");
+                int nomorMenu = Convert.ToInt32(Console.ReadLine());
+
+                switch (nomorMenu)
+                {
+                    case 1:
+                        TambahMahasiswa();
+                        break;
+
+                    case 2:
+                        TampilMahasiswa();
+                        break;
+
+                    case 3: // keluar dari program
+                        return;
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+        static void TampilMenu()
+        {
+            Console.Clear();
+
+            // PERINTAH: lengkapi kode untuk menampilkan menu
+        }
+
+        static void TambahMahasiswa()
+        {
+            Console.Clear();
+
+            // PERINTAH: lengkapi kode untuk menambahkan objek mahasiswa ke dalam collection
+
+            Console.WriteLine("\nTekan ENTER untuk kembali ke menu");
+            Console.ReadKey();
+        }
+
+        static void TampilMahasiswa()
+        {
+            Console.Clear();
+
+            // PERINTAH: lengkapi kode untuk menampilkan daftar mahasiswa yang ada di dalam collection
+
+            Console.WriteLine("\nTekan enter untuk kembali ke menu");
+            Console.ReadKey();
+        }
+    }
+}
